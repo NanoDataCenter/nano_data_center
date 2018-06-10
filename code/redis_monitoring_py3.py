9d6fa318b995d20493958a043856a25774cb14e8
Glenn Edgar <glenn-edgar@onyxengr.com>
2018-06-10 07:11:37 -0700

    daily commit

--- code/redis_monitoring_py3.py
+++ code/redis_monitoring_py3.py
@@ -7,24 +7,54 @@ class Redis_Monitor(object):
    def __init__(self,redis_handle, redis_monitoring_streams ):
        self.redis_handle = redis_handle
        self.redis_monitoring_streams = redis_monitoring_streams
+       self.cpu_previous = None
+      
+       self.call_stat_previous = None
        
        
    def log_data(self,*parameters):
        
        redis_data = self.redis_handle.info("Keyspace")     
-
        self.redis_monitoring_streams["KEYS"].push(data=redis_data)      
+       
        redis_data = self.redis_handle.info("Clients") 
-             
-       self.redis_monitoring_streams["CLIENTS"].push(data=redis_data)      
-       redis_data = self.redis_handle.info("Memory")      
-      
+       self.redis_monitoring_streams["CLIENTS"].push(data=redis_data)  
+       
+       redis_data = self.redis_handle.info("Memory")            
        self.redis_monitoring_streams["MEMORY"].push(data=redis_data)      
-       #print(self.redis_monitoring_streams["KEYS_READER"].xrange_compress("-","+"))
-       #print(self.redis_monitoring_streams["KEYS_READER"].xrevrange_compress("+","-"))
 
-
-
+       redis_data = self.redis_handle.info("commandstats")
+       temp_time = {}
+       temp_calls = {}
+       for k, item in redis_data.items():
+          temp_time[ k] = item[ 'usec_per_call']
+          temp_calls[k]  = item['calls']
+          
+       self.redis_monitoring_streams["REDIS_MONITOR_CMD_TIME_STREAM"].push(data=temp_time)     
+       if self.call_stat_previous != None:
+           delta_call = {}
+           for k,item in temp_calls.items():
+               delta_call[k] = item - self.call_stat_previous[k]
+           print("call stat ",delta_call)
+           self.redis_monitoring_streams["REDIS_MONITOR_CALL_STREAM"].push(data=delta_call)  
+       
+       redis_data = self.redis_handle.info("cpu")
+       
+       if self.cpu_previous != None:
+          temp = {}
+          for key, item in redis_data.items():
+              temp[key] = float(item) - self.cpu_previous[key]
+          print(temp)
+          self.redis_monitoring_streams["REDIS_MONITOR_SERVER_TIME"].push(data=temp)  
+       else:
+          temp = redis_data
+          
+       
+       self.call_stat_previous = temp_calls
+       self.cpu_previous = redis_data
+       
+ 
+       
 def construct_redis_instance( qs, site_data ):
 
                    
@@ -51,8 +81,10 @@ def construct_redis_instance( qs, site_data ):
     redis_monitoring_streams["KEYS"] = generate_handlers.construct_stream_writer(data_structures["REDIS_MONITOR_KEY_STREAM"] )
     redis_monitoring_streams["CLIENTS"] = generate_handlers.construct_stream_writer(data_structures["REDIS_MONITOR_CLIENT_STREAM"] )
     redis_monitoring_streams["MEMORY"] = generate_handlers.construct_stream_writer(data_structures["REDIS_MONITOR_MEMORY_STREAM"] )
-    redis_monitoring_streams["KEYS_READER"] = generate_handlers.construct_stream_reader(data_structures["REDIS_MONITOR_KEY_STREAM"] )
-
+    
+    redis_monitoring_streams["REDIS_MONITOR_CALL_STREAM"] = generate_handlers.construct_stream_writer(data_structures["REDIS_MONITOR_CALL_STREAM"])
+    redis_monitoring_streams["REDIS_MONITOR_CMD_TIME_STREAM"] = generate_handlers.construct_stream_writer(data_structures["REDIS_MONITOR_CMD_TIME_STREAM"])
+    redis_monitoring_streams["REDIS_MONITOR_SERVER_TIME"] = generate_handlers.construct_stream_writer(data_structures["REDIS_MONITOR_SERVER_TIME"])
  
    
     redis_handle = generate_handlers.get_redis_handle()  # reusing handle from package
--- construct_graph_py3/graph_modules_py3/construct_redis_monitor_py3.py
+++ construct_graph_py3/graph_modules_py3/construct_redis_monitor_py3.py
@@ -16,6 +16,9 @@ class Construct_Redis_Monitoring(object):
        cd.add_stream("REDIS_MONITOR_KEY_STREAM",depth=ONE_MONTH)
        cd.add_stream("REDIS_MONITOR_CLIENT_STREAM",depth=ONE_MONTH)
        cd.add_stream("REDIS_MONITOR_MEMORY_STREAM",depth=ONE_MONTH)
+       cd.add_stream("REDIS_MONITOR_CALL_STREAM",depth=ONE_MONTH)
+       cd.add_stream("REDIS_MONITOR_CMD_TIME_STREAM",depth=ONE_MONTH)
+       cd.add_stream("REDIS_MONITOR_SERVER_TIME",depth = ONE_MONTH)
        
        cd.close_package_contruction()
           
